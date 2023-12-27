9b1bd2df11c3a692980a3a10ef4891a5c7582c62
felipefo <felipefo@users.noreply.github.com>
2023-12-27 18:25:51 -0300

    Calculando a idade do somovente e criando organização do sistema
    Calculando a idade do somovente e criando organização do sistema

--- backend/app1/src/api/animal/controllers/animal.ts
+++ backend/app1/src/api/animal/controllers/animal.ts
@@ -4,4 +4,42 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::animal.animal');
+export default factories.createCoreController("api::animal.animal",
+  ({ strapi }) => ({
+
+    async find(ctx) {
+        let  { data, meta } = await super.find(ctx);
+       // some more logic
+       data.forEach(item => {
+        console.warn(item);
+        console.warn(item.Data_nascimento);
+        item.attributes.Idade = calcularIdade(item.attributes.Data_nascimento);
+       });
+       
+       //console.warn(data);
+       return { data , meta };
+    },
+  })
+
+);
+
+
+function calcularIdade(dataNascimento) {
+    console.warn(dataNascimento);
+    if (!dataNascimento) {
+        return null; // Retorna null se a data de nascimento for nula
+    }
+    const dataNascimentoObj = new Date(dataNascimento);
+    const dataAtual = new Date();
+    const diferencaEmAnos = dataAtual.getFullYear() - dataNascimentoObj.getFullYear();
+    // Ajuste para casos em que o aniversário ainda não ocorreu este ano
+    if (
+        dataNascimentoObj.getMonth() > dataAtual.getMonth() ||
+        (dataNascimentoObj.getMonth() === dataAtual.getMonth() && dataNascimentoObj.getDate() > dataAtual.getDate())
+    ) {
+        return diferencaEmAnos - 1;
+    }
+
+    return diferencaEmAnos;
+}
+
